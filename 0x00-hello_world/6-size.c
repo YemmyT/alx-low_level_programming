3d2620801a8fbc8475d9fc36b74ab82c6f7c9b45
YemmyT <toyinyemmyast@gmail.com>
2022-06-30 17:48:06 -0700

    task_6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,16 +2,17 @@
 
 
 /**
- * main - print the size of various types
- * Return: 0 if exited properply, non-zero otherwise
- */
+ * main -print the size of various types based on 
+ * the computerit comiled on
+ * Return: Always 0.
+*/
  int main(void)
 {
-printf("size of a char: %d byte(s)\n", sizeof (char));
-printf("size of an int: %d byte(s)\n", sizeof (int));
-printf("size of a long int: %d byte(s)\n", sizeof (long int));
-printf("size of a long long int: %d byte(s)\n", sizeof (long long int));
-printf("size of a float: %d byte(s)\n", sizeof(float))
+printf("size of a char: %ty byte(s)\n", sizeof (char));
+printf("size of an int: %ty byte(s)\n", sizeof (int));
+printf("size of a long int: %ty byte(s)\n", sizeof (long int));
+printf("size of a long long int: %ty byte(s)\n", sizeof (long long int));
+printf("size of a float: %ty byte(s)\n", sizeof(float));
 
 return (0);
 
