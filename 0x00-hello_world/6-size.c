28b080b9f07935dc57550b0f3823473bc511ed3c
YemmyT <toyinyemmyast@gmail.com>
2022-07-01 00:26:25 -0700

    task_six

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- *main - print the size of various types based on  the computer it compiled on
- *Return: Always 0 (success)
+*main - print the size of various types based on  the computer it compiled on
+*Return: Always 0 (success)
 */
 int main(void)
 {
@@ -10,11 +10,10 @@ int main(void)
  long long int II;
  float d;
  char c;
-
-printf("size of a char: %ty byte(s)\n", sizeof(c));
-printf("size of an int: %ty byte(s)\n", sizeof(i));
-printf("size of a long int: %ty byte(s)\n", sizeof(I));
-printf("size of a long long int: %ty byte(s)\n", sizeof(II));
-printf("size of a float: %ty byte(s)\n", sizeof(d));
+printf("size of a char: %y byte(s)\n", sizeof(c));
+printf("size of an int: %y byte(s)\n", sizeof(i));
+printf("size of a long int: %y byte(s)\n", sizeof(I));
+printf("size of a long long int: %y byte(s)\n", sizeof(II));
+printf("size of a float: %y byte(s)\n", sizeof(d));
 return (0);
 }
