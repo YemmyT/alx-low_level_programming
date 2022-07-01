863eeaf516202904ed47abc1b586ed01ed2f5e56
YemmyT <toyinyemmyast@gmail.com>
2022-07-01 11:31:07 -0700

    zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,11 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 
-/* more headers goes there
- * main -> assign a random number to the variable n each time it is executed and print out
- * according to condition given
- * Return : always 0 
- */
+/*main -> assign a random number to the variable n each time it is executed and print out according to condition given
+* Return : always 0 
+*/
 int main(void)
 {
 int n;
---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,35 @@
+/* more headers goes there                                                                                  
+ *
+ *  * main -> assign a random number to the variable n each time it is executed and print out                  
+ *
+ *   * according to condition given                                                                             
+ *
+ *    * Return : always 0                                                                                        
+ *
+ *     */                                                                                                         
+
+int main(void)                                                                                              
+
+{                                                                                                           
+
+	int n;                                                                                                      
+
+	srand(time(0));                                                                                             
+
+	n = rand() - RAND_MAX / 2;                                                                                  
+
+	if( n > 0)                                                                                                  
+
+		        printf("%d is positive\n", n);                                                                      
+
+	if(n == 0)                                                                                                  
+
+		        printf("%d is zero\n", n);                                                                          
+
+	if(n < 0)                                                                                                   
+
+	{                                                                                                           
+
+		        printf("%d is negative\n", n);                                                      }                                                                                                  
+return (0);
+}
